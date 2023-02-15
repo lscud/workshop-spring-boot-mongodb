b9af4c84cd13fe7abbde6d0ae6ecd52551ff5e57
Luis Antonio <luisscudeler@yahoo.com.br>
2023-02-15 14:28:43 -0300

    DTO implements

--- src/main/java/com/luisantonio/workshopmongo/resources/UserResource.java
+++ src/main/java/com/luisantonio/workshopmongo/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.luisantonio.workshopmongo.resources;
 
 import com.luisantonio.workshopmongo.Services.UserService;
 import com.luisantonio.workshopmongo.domain.User;
+import com.luisantonio.workshopmongo.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 //O back end vai expor os controladores RETS para as aplicações. Os controladores REST vao acessar os serviços
 // e os serviços irão acessar os objetos da camada de de acesso a dados (REPOSITORY: Objetos responsaveis por efetuar operações basicas de acesso a dados)
@@ -24,8 +26,13 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET) //Poderia ter usado o @GetMapping
-    public ResponseEntity<List<User>> findAll(){  //Poderia retornar apenas a List<User> no entanto o ResponseEntity é um recurso mais sofisticado com recursos HTTP
+    public ResponseEntity<List<UserDTO>> findAll(){  //Poderia retornar apenas a List<User> no entanto o ResponseEntity é um recurso mais sofisticado com recursos HTTP
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list); //.ok é um metodo que vai instanciar o responserentity ja com a resposta http respondendo que ocorreu com sucesso
+
+        //list.stream irá converter num streming que é uma coleção compativel com as expreções lambda do java 8 em diante
+        //map(x -> new UserDTO(x)) o map vai pegar cada elemento X da minha lista original e pra cada elemento x que é um User eu vou retornar um new UserDTO passando x como argumento
+        //collect((Collectors.toList()) é para converter novamente o streaming para lista
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect((Collectors.toList()));
+        return ResponseEntity.ok().body(listDto); //.ok é um metodo que vai instanciar o responserentity ja com a resposta http respondendo que ocorreu com sucesso
     }
 }
