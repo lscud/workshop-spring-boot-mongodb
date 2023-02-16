8aa5e7127d31a08f7073dfc32dd481abc7665b25
Luis Antonio <luisscudeler@yahoo.com.br>
2023-02-16 13:55:53 -0300

    Simple find with @Query

--- src/main/java/com/luisantonio/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/luisantonio/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,18 @@ package com.luisantonio.workshopmongo.repository;
 import com.luisantonio.workshopmongo.domain.Post;
 import com.luisantonio.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {  //funções como salvar, deletar, alterar ja está imbutido nesse MongoRepository
+
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
