dacdb530b5d4b535df149cc0af062275e3b9ac21
Luis Antonio <luisscudeler@yahoo.com.br>
2023-02-16 13:43:53 -0300

    find Query Method

--- src/main/java/com/luisantonio/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/luisantonio/workshopmongo/repository/PostRepository.java
@@ -5,6 +5,10 @@ import com.luisantonio.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {  //funções como salvar, deletar, alterar ja está imbutido nesse MongoRepository
+    List<Post> findByTitleContainingIgnoreCase(String text);
+
 }
--- src/main/java/com/luisantonio/workshopmongo/resources/PostResource.java
+++ src/main/java/com/luisantonio/workshopmongo/resources/PostResource.java
@@ -5,6 +5,7 @@ import com.luisantonio.workshopmongo.Services.UserService;
 import com.luisantonio.workshopmongo.domain.Post;
 import com.luisantonio.workshopmongo.domain.User;
 import com.luisantonio.workshopmongo.dto.UserDTO;
+import com.luisantonio.workshopmongo.resources.util.URL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,4 +32,12 @@ public class PostResource {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @RequestMapping(value="/titlesearch", method = RequestMethod.GET) //Poderia ter usado o @GetMapping
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
 }
